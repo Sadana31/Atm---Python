0810623e3057840a7c62fbfb9a75e934a7f7e69b
Sadana31 <63460528+Sadana31@users.noreply.github.com>
2020-12-03 21:15:33 +0530

    Add files via upload

--- Atm.py
+++ Atm.py
@@ -1,27 +1,50 @@
-class atm(object):
-    def __init__(self,exisitingAmount,pin,cashWithdrawn):
-        self.exisitingAmount = exisitingAmount
-        self.pin = pi
-        self.cashWithdrawn = cashWithdrawn
-        self.currentBalance = 5000.00
-
-    def intro(self):
-        print("Welcome to Sadana's ATM")
-        print("Here, you can withdraw cash from your existing account")
-        print("And, check your exisiting balance")
-
-    def enterPin(self):
-        input("Enter your PIN")
+class atm:
+    def __init__(self,pin):
+        self.pin = pin
 
     def checkBalance(self):
-        print("Your current balance: INR ", currentBalance)
+        print("Your current balance is INR 50000")
 
-    def withdrawCash(self):
-        cash = input("Enter the amount to be withdrawn: ")
-        newCurrentBalance = currentBalance - cash
-        print(cash, "INR has been withdrawn from your account")
-        print("Thanks for using Sadana's atm :)")
+    def withdrawCash(self,amount):
+        newAmount = 50000 - amount
+        print("You have withdrawn"+str(amount) + ". Your remaining balance is "+ str(newAmount))
+        print("Thank you for banking with me :)")
 
-    at = atm(5000,3125,200)
-    at.intro()
+    def depositCash(self,amount):
+        newAmount = 50000 + amount
+        print("You have deposited " + str(amount) + ". You current balance is " + str(newAmount))
+        print("Thank you for banking with me :)")
 
+def main():
+    # Card_number = input("insert your card number:- ")
+
+    print("")
+    print("Welcome to Sadana's ATM!")
+
+    pin_number  = input("Enter your pin number: ")
+    user =  atm(pin_number)
+
+    print("")
+    print("Choose: ")
+    print("1 for Balance Enquriy")
+    print("2 for Cash withdrawal")
+    print("3 for cash deposit")
+    print("")
+    activity = int(input("Enter your activity number: "))
+
+    if (activity == 1):
+        user.checkBalance()
+    elif (activity == 2):
+        print("")
+        amount = int(input("Enter the amount to be withdrawn: "))
+        user.withdrawCash(amount)
+    elif (activity == 3):
+        print("")
+        amount = int(input("Enter the amount to be deposited: "))
+        user.depositCash(amount)
+    else:
+        print("enter a valid number")
+
+
+if __name__ == "__main__":
+    main()
